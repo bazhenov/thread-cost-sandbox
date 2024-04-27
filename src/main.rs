cafb49828ada5c531bf4c0cb7786e49f73d4d933
Denis Bazhenov <dotsid@gmail.com>
2024-04-27 13:12:47 +0200

    Initial version

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use std::sync::{Arc, Barrier};
+
+use memory_stats::{memory_stats, MemoryStats};
+
+fn main() {
+    const THREADS: usize = 1000;
+
+    // Warmup
+    measure_memory_per_thread(THREADS, small_thread);
+
+    let usage = measure_memory_per_thread(THREADS, small_thread);
+    report_usage("small_thread", usage);
+
+    let usage = measure_memory_per_thread(THREADS, large_thread::<{ 1024 * 1024 }>);
+    report_usage("large_thread (1M)", usage);
+}
+
+fn report_usage(name: &str, usage_per_thread: MemoryStats) {
+    println!("=== Memory usage per thread : {} ===", name);
+    println!("  Physical: {:>10}", usage_per_thread.physical_mem);
+    println!("   Virtual: {:>10}", usage_per_thread.virtual_mem);
+}
+
+fn measure_memory_per_thread(threads: usize, f: fn(Arc<Barrier>)) -> MemoryStats {
+    // Inclusing outselves in waiting group to be able to block thread in running state
+    let barrier = Arc::new(Barrier::new(threads + 1));
+    let mut handles = vec![];
+
+    let usage_before = memory_stats().unwrap();
+    for _ in 0..threads {
+        let barrier = Arc::clone(&barrier);
+        let handle = std::thread::spawn(move || f(barrier));
+        handles.push(handle);
+    }
+    let usage_after = memory_stats().unwrap();
+
+    barrier.wait();
+    for handle in handles.into_iter() {
+        handle.join().unwrap();
+    }
+
+    let physical_mem = (usage_after.physical_mem - usage_before.physical_mem) / threads;
+    let virtual_mem = (usage_after.virtual_mem - usage_before.virtual_mem) / threads;
+    MemoryStats {
+        physical_mem,
+        virtual_mem,
+    }
+}
+
+fn small_thread(barrier: Arc<Barrier>) {
+    barrier.wait();
+}
+fn large_thread<const S: usize>(barrier: Arc<Barrier>) {
+    let _blob: [u8; S] = [0; S];
+    barrier.wait();
+}
